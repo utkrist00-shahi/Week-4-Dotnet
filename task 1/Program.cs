e870e57d37bac2a9545578262b95e20249c9eda8
utkrist00-shahi <utkristshahi56@gmail.com>
2025-11-19 11:45:06 +0545

    task 1 completed and initialized git

---
+++ task 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using task_1;
+using System;
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+
+        Student stud1 = new Student();
+        Student stud2 = new Student();
+
+        Console.WriteLine(stud1.studentName);
+        Console.WriteLine(stud2.studentRoll);
+        Console.WriteLine(stud1.studentAge);
+
+     
+
+
+
+    }
+}
---
+++ task 1/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1
+{
+    internal class Student
+    {
+        public  String studentName = "Utkrist";
+        public int studentRoll = 10;
+        public int studentAge = 19;
+
+
+        public static String studentAdress = "Biratchowk";
+    }
+}
