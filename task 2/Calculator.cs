ba0d7547c5e41ec53f89e7207ccdd6de30b59df4
utkrist00-shahi <utkristshahi56@gmail.com>
2025-11-19 12:09:44 +0545

    Task 2 completed

---
+++ task 2/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2
+{
+    internal class Calculator
+    {
+        public void PrintWelcome() {
+
+            Console.WriteLine("Welcome to the Calculator");
+        }
+        public int Add(int num1, int num2) {
+        
+        
+        return num1 + num2;
+
+        }
+        public int Multiply( int num1 , int num2 = 1)
+        {
+
+           
+            return num1 * num2;
+
+        }
+    }
+}
---
+++ task 2/Program.cs
@@ -0,0 +1,20 @@
+﻿using task_2;
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        Calculator cal = new Calculator();
+        cal.PrintWelcome();
+        int add = cal.Add(1, 2);
+        int multi = cal.Multiply(5);
+        Console.WriteLine(add);
+        Console.WriteLine(multi);
+         
+    }
+
+
+    
+    }
+
