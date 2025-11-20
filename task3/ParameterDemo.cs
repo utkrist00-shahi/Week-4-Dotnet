e011d7f53bae5e6d57297cb81ee57e85625ed185
utkrist00-shahi <utkristshahi56@gmail.com>
2025-11-20 09:10:09 +0545

    task 3 completed

---
+++ task3/ParameterDemo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class ParameterDemo
+{
+    public void Increase(ref int number)
+    {
+        number = number + 10;
+    }
+
+    public void GetFullName(out string fullName)
+    {
+        fullName = "Utkrist Shahi";
+    }
+
+    public int SumAll(params int[] numbers)
+    {
+        int total = 0;
+        foreach (int num in numbers)
+        {
+            total += num;
+        }
+        return total;
+    }
+}
---
+++ task3/Program.cs
@@ -0,0 +1,21 @@
+﻿ class Program
+{
+    static void Main(string[] args)
+    {
+        ParameterDemo demo = new ParameterDemo();
+
+        int value = 10;
+        Console.WriteLine("Before Increase: " + value);
+        demo.Increase(ref value);
+        Console.WriteLine("After Increase: " + value);
+
+        string name;
+        demo.GetFullName(out name);
+        Console.WriteLine("Full Name: " + name);
+
+        int sum = demo.SumAll(10, 20, 30, 40, 50, 5, 15);
+        Console.WriteLine("Sum of numbers: " + sum);
+
+        Console.ReadLine();
+    }
+}
