8db4efc7b1def883ea2d5e8e687d2e6072db1ceb
utkrist00-shahi <utkristshahi56@gmail.com>
2025-11-20 11:19:19 +0545

    task 6 completed

---
+++ task 6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter marks: ");
+        int marks;
+        int.TryParse(Console.ReadLine(), out marks);
+
+        Console.Write("Enter total: ");
+        int total;
+        int.TryParse(Console.ReadLine(), out total);
+
+        double percentage = (double)marks / total* 100.0;  
+
+        Console.WriteLine("Percentage = " + percentage);
+        Console.ReadLine();
+    }
+}
