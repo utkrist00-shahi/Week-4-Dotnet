05f96d165c3b5eb1921e59b250bd902ebe55bdf5
utkrist00-shahi <utkristshahi56@gmail.com>
2025-11-20 09:17:23 +0545

    task 4 completed

---
+++ task 4/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Player
+{
+    string playerName;
+    int level;
+    int health;
+
+    public Player()
+    {
+        Console.WriteLine("Default constructor");
+    }
+
+    public Player(string name, int lvl, int hp)
+    {
+        playerName = name;
+        level = lvl;
+        health = hp;
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Name: {playerName}, Level: {level}, Health: {health}");
+    }
+}
---
+++ task 4/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Player p1 = new Player();
+
+        Player p2 = new Player("Utkrist", 30, 100);
+
+        p1.PrintInfo();
+        p2.PrintInfo();
+
+        Console.ReadLine();
+    }
+}
